eccd768e22ac53a018e8939d8bfe78fb27789162
gengxin.wang <1160183812@qq.com>
2024-05-20 17:02:48 +0800

    MainOpenFeign80 补提

---
+++ cloud-consumer-feign-order80/src/main/java/com/cloud/MainOpenFeign80.java
@@ -0,0 +1,22 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ *
+ * @author gengxin.wang
+ * @date 2024/5/15 10:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul为注册中心时注册服务
+@EnableFeignClients//启用feign客户端,定义服务+绑定接口，以声明式的方法优雅而简单的实现服务调用
+public class MainOpenFeign80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MainOpenFeign80.class,args);
+    }
+}
